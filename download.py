37eecfad79247aec4a4a4fcabca8be88856cf4f6
jxl <361425474@qq.com>
2019-02-08 17:51:46 +0800

    添加代码

--- download.py
+++ download.py
@@ -20,9 +20,31 @@ def get_movie_list_url(base_url):
         print 'movie_list_url is (null)'
         return ''
 
+# 对下载失败的url进行一定次数的重试
+def retry_downloadfile(retry_list, retry_times):
+    if len(retry_list) == 0:
+        print 'all urls download success'
+        return
+    if retry_times == 0:
+        print 'url download fail list:'
+        for url in retry_list:
+            print url
+        return
+    retry_list_new = []
+    for url in retry_list:
+        try:
+            r = requests.get(url=url, timeout=(3.5, 30))
+            with open(url, 'wb') as f:
+                f.write(r.content)
+        except Exception, e:
+            retry_list_new.append(url)
+            print type(e), ':', e
+    retry_downloadfile(retry_list_new, retry_times - 1)
+
 # 根据电影的url列表下载每个视频片段
 def downloadfile(movie_list_url,out_dir_name):
     filename_pre_str = os.getcwd() + '/' + out_dir_name + '/'
+    retry_list = []
     for line in urllib.urlopen(movie_list_url):
         line = line.strip()
         line.replace('  ', ' ')
@@ -35,7 +57,9 @@ def downloadfile(movie_list_url,out_dir_name):
                 with open(filename_pre_str + filename, 'wb') as f:
                     f.write(r.content)
             except Exception, e:
+                retry_list.append(base_str + line)
                 print type(e), ':', e
+    retry_downloadfile(retry_list, 10)
 
 # windows下合并多个文件到一个文件
 def combine_files(out_dir_name):
@@ -53,7 +77,7 @@ def download(base_url,out_dir_name):
         movie_list_url = get_movie_list_url(base_url)
         if movie_list_url != '':
             downloadfile(movie_list_url, out_dir_name)
-            combine_files(out_dir_name)
+            #combine_files(out_dir_name)
     except Exception, e:
         print type(e), ':', e
 
@@ -85,10 +109,10 @@ if __name__ == '__main__':
     #download('https://cn2.zuidadianying.com/20190207/Nj4iG5WQ/index.m3u8', 'lldq') # 流浪地球
     #get_movie_url_for_thunder('https://cn2.zuidadianying.com/20190207/Nj4iG5WQ/index.m3u8', 'lldq.txt') # 流浪地球
 
-    download('https://cn2.zuidadianying.com/20190207/HAirIKlM/index.m3u8', 'fkdwxr')  # 疯狂的外星人
+    #download('https://cn2.zuidadianying.com/20190207/HAirIKlM/index.m3u8', 'fkdwxr')  # 疯狂的外星人
     #get_movie_url_for_thunder('https://cn2.zuidadianying.com/20190207/HAirIKlM/index.m3u8', 'fkdwxr.txt') # 疯狂的外星人
 
-    #download('https://cn2.zuidadianying.com/20190207/7hFXzH8Z/index.m3u8', 'fcrs')  # 飞驰人生
+    download('https://cn2.zuidadianying.com/20190207/7hFXzH8Z/index.m3u8', 'fcrs')  # 飞驰人生
     #get_movie_url_for_thunder('https://cn2.zuidadianying.com/20190207/7hFXzH8Z/index.m3u8', 'fcrs.txt') # 飞驰人生
 
     #download('https://cn2.zuidadianying.com/20190207/Nj4iG5WQ/index.m3u8', 'xxjzw')  # 新喜剧之王
